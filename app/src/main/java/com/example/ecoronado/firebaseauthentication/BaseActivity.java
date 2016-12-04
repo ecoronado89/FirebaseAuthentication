72a76a1b914625c3e131ea7fbfdae8459afa7f35
Didier Corrales <dcorrales@llbean.com>
2016-12-04 02:27:26 -0600

    Changing the use for base activity

---
+++ app/src/main/java/com/example/ecoronado/firebaseauthentication/BaseActivity.java
@@ -0,0 +1,34 @@
+package com.example.ecoronado.firebaseauthentication;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by david on 11/1/16.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+
+    protected Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResource());
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+        }
+
+
+    }
+
+    @LayoutRes
+    protected abstract int getLayoutResource();
+
+}
