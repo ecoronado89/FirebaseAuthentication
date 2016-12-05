6e3f53152a09b99be717b6dcf946390a8db63abf
Didier Corrales <dcorrales@llbean.com>
2016-12-04 19:27:41 -0600

    adding Chat activity and add functionality to move between the activities

--- app/src/main/java/com/example/ecoronado/firebaseauthentication/ChatActivity.java
+++ app/src/main/java/com/example/ecoronado/firebaseauthentication/ChatActivity.java
@@ -1,8 +1,44 @@
 package com.example.ecoronado.firebaseauthentication;
 
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+
 /**
  * Created by llbean on 12/4/16.
  */
 
-public class ChatActivity {
+public class ChatActivity extends BaseActivity{
+
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResource());
+
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+         getSupportActionBar().setHomeButtonEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.chat_activity;
+    }
 }
--- app/src/main/java/com/example/ecoronado/firebaseauthentication/ContactListActivity.java
+++ app/src/main/java/com/example/ecoronado/firebaseauthentication/ContactListActivity.java
@@ -1,11 +1,13 @@
 package com.example.ecoronado.firebaseauthentication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class ContactListActivity extends BaseActivity {
+public class ContactListActivity extends BaseActivity implements View.OnClickListener {
 
 
 
@@ -14,6 +16,8 @@ public class ContactListActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(getLayoutResource());
 
+        Button button = (Button) findViewById(R.id.button_holder);
+        button.setOnClickListener(this);
 
         // TODO Put the home icon
          //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -24,6 +28,14 @@ public class ContactListActivity extends BaseActivity {
     @Override
     protected int getLayoutResource() {
         return R.layout.contact_list_layout;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.button_holder){
+            startActivity(new Intent(ContactListActivity.this,ChatActivity.class));
+            //finish();
+        }
     }
 
 }
